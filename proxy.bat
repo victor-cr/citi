4d6c77c97dd68e260daf8e99ba586651274dc70b
Viktor <victor2@ukr.net>
2026-08-06 13:57:10 +0300

    Added python modules installation

--- proxy.bat
+++ proxy.bat
@@ -2,7 +2,9 @@
 
 set CURRENT_DIR=%~dp0
 mkdir %CURRENT_DIR%data
-cd CURRENT_DIR
+cd %CURRENT_DIR%
 
 git pull -r
+python -m pip install --upgrade pip
+python -m pip install pyyaml regex requests
 python "%CURRENT_DIR%plugin.proxy.py" "%CURRENT_DIR%data"
